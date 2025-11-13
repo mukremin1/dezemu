aab3be2dfe7bef8a2a286c5e39de79b1a74d9e48
mukremin1 <115407114+mukremin1@users.noreply.github.com>
2025-11-13 23:33:53 +0300

    Import SearchPage component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import FAQ from "./pages/FAQ";
 import Shipping from "./pages/Shipping";
 import AdminUpload from "./pages/AdminUpload";
 import NotFound from "./pages/NotFound";
+import SearchPage from "@/pages/SearchPage";
 
 const queryClient = new QueryClient();
 
