80398c61be71e55c257ad0d0f4e73df673cd6589
mukremin1 <115407114+mukremin1@users.noreply.github.com>
2025-11-14 00:12:27 +0300

    Update import paths and add HomePage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Header } from "./components/Header";
-import { SearchPage } from "./pages/SearchPage";
+import { Header } from "@/components/Header";
+import { SearchPage } from "@/pages/SearchPage";
+import HomePage from "@/pages/HomePage"; // kendi ana sayfan
 
 export const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
-        {/* Diğer sayfalar */}
       </Routes>
     </Router>
   );
