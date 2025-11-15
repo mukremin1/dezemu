f7e62ef82595810475a89fbbb2251acefa14a291
mukremin1 <115407114+mukremin1@users.noreply.github.com>
2025-11-15 19:41:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,22 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // GitHub Pages veya custom domain için root
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
       "@utils": path.resolve(__dirname, "src/utils"),
       "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
   build: {
-    sourcemap: true, // hata ayıklamak için
+    sourcemap: true,
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
   },
+  base: "/", // GitHub Pages için "/" veya "/dezemu/" (repo adı ile eşleşmeli)
 });
