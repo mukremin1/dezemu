d9046ba0353e7d972b3851769e97e2f6cf0693e3
mukremin1 <115407114+mukremin1@users.noreply.github.com>
2025-11-15 16:49:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Header } from "@/components/Header";
+import  Header  from "@/components/Header";
 import { SearchPage } from "@/pages/SearchPage";
 import HomePage from "@/pages/HomePage";
 
